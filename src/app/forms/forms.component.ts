54c37fd9454bce94b75dd04ffe46a140d5411397
sri-pusuluri <srini@zool.in>
2019-03-20 16:49:16 +0530

    modified variable file, removed spacing from buttons, added inside modal-actions for modal button spacing, radiobutton class placment change, checkbox id,for added

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -16,9 +16,9 @@ export class FormsComponent implements OnInit {
       title: 'Checkbox',
       demoCode: `
       <div class="sb-checkbox sb-checkbox-primary">
-    <input type="checkbox" name="example">
-    <label>Make my profile visible</label>
-</div>
+      <input type="checkbox" id="check1" name="example">
+      <label for="check1">Make my profile visible</label>
+      </div>
       `,
       copyCode: `
       <div class="sb-checkbox sb-checkbox-primary">
@@ -33,7 +33,7 @@ export class FormsComponent implements OnInit {
       demoCode: `
       
       <div class="sb-field-group">
-      <label>Input with icon</label>
+      <label>Field Name</label>
       <div class="sb-field">
           <input class="sb-form-control" disabled type="text" placeholder="Enter Mobile Number">
       </div>
@@ -43,7 +43,7 @@ export class FormsComponent implements OnInit {
       copyCode: `
       
       <div class="sb-field-group">
-      <label>Input with icon</label>
+      <label>Field Name</label>
       <div class="sb-field">
           <input class="sb-form-control" disabled type="text" placeholder="Enter Mobile Number">
       </div>
@@ -57,7 +57,7 @@ export class FormsComponent implements OnInit {
       demoCode: `
       
       <div class="sb-field-group">
-      <label>Input with icon</label>
+      <label>Field Name</label>
       <div class="sb-field">
           <input class="sb-form-control is-invalid">
       </div>
@@ -68,7 +68,7 @@ export class FormsComponent implements OnInit {
       copyCode: `
       
       <div class="sb-field-group">
-      <label>Input with icon</label>
+      <label>Field Name</label>
       <div class="sb-field">
           <input class="sb-form-control is-invalid">
       </div>
@@ -79,11 +79,11 @@ export class FormsComponent implements OnInit {
     },
     {
       expandCode: false,
-      title: 'Input with Icon',
+      title: 'Field Name',
       demoCode: `
       
       <div class="sb-field-group">
-      <label>Input with icon</label>
+      <label>Field Name</label>
       <div class="sb-field">
           <input class="sb-form-control" type="text" placeholder="Enter Mobile Number">
           <i class="search icon sb-input-icon"></i>
@@ -94,7 +94,7 @@ export class FormsComponent implements OnInit {
       copyCode: `
       
       <div class="sb-field-group">
-      <label>Input with icon</label>
+      <label>Field Name</label>
       <div class="sb-field">
           <input class="sb-form-control" type="text" placeholder="Enter Mobile Number">
           <i class="search icon sb-input-icon"></i>
@@ -156,19 +156,17 @@ export class FormsComponent implements OnInit {
       expandCode: false,
       title: 'Radio',
       demoCode: `
-      <form action="#" class="sb-radio-btn">
-      <div class="sb-radio sb-radio-primary">
+      <div class="sb-radio-btn sb-radio-primary">
         <input type="radio" id="test1" name="radio-group" disabled="disabled" checked>
         <label for="test1">Radio Button label</label>
       </div>
-      <div class="sb-radio sb-radio-primary">
+      <div class="sb-radio-btn sb-radio-primary">
         <input type="radio" id="test2" name="radio-group">
         <label for="test2">Peach</label>
       </div>
     
       `,
       copyCode: `
-      <form action="#" class="sb-radio-btn">
       <div class="sb-radio sb-radio-primary">
         <input type="radio" id="test1" name="radio-group" disabled="disabled" checked>
         <label for="test1">Radio Button label</label>
@@ -177,31 +175,26 @@ export class FormsComponent implements OnInit {
         <input type="radio" id="test2" name="radio-group">
         <label for="test2">Peach</label>
       </div>
-    
       `
     },
     {
       expandCode: false,
       title: 'Text Area',
       demoCode: `
-      
       <div class="sb-field-group">
       <label>Field Label</label>
           <div class="sb-field">
               <textarea rows="4" class="sb-form-control" placeholder="Enter summary"></textarea>
           </div>
           </div>
-      
       `,
       copyCode: `
-      
       <div class="sb-field-group">
       <label>Field Label</label>
           <div class="sb-field">
               <textarea rows="4" class="sb-form-control" placeholder="Enter summary"></textarea>
           </div>
           </div>
-      
       `
     }
 
