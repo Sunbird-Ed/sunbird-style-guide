791c919f8bce1d684a157f340b734000637d85db
prajwal88 <prajwal.hj@compassitesinc.com>
2019-04-16 17:44:52 +0530

    creating minified file for each component file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+    sass = require('gulp-sass');
+    sourcemaps = require('gulp-sourcemaps');
+    rename = require('gulp-rename');
+    path = require('path');
+    sassPartialsImported = require('gulp-sass-partials-imported');
+    fs = require("fs");
+
 
 //style paths
 var sassFiles = 'sb-components.scss',
@@ -13,3 +19,25 @@ gulp.task('styles', function(){
 gulp.task('watch',function() {
     gulp.watch(sassFiles,['styles']);
 });
+
+
+gulp.task('sass', function () {
+    return gulp
+      .src('./src/assets/styles/component/**/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(rename(function(path) {
+        path.basename = path.basename[0] == '_' ? path.basename.substr(1) : path.basename
+      }))
+      .pipe(sass({
+        outputStyle: 'compressed'
+      }).on('error', sass.logError))
+      .pipe(sourcemaps.write('./'))
+       
+       // rename the current file's parent directory
+
+      .pipe(rename(function (file) {
+        let parentFolder = path.dirname(file.dirname)
+        file.dirname = path.join(parentFolder, 'css');
+      }))
+      .pipe(gulp.dest('dist/component-list'));
+  });
