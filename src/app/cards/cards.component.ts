72783fe047699660769ff970531bb9b315fe95bb
vinodkumar303 <vinodkumar@zool.in>
2019-05-10 15:38:48 +0530

    batch component structure change

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -609,106 +609,92 @@ export class CardsComponent implements OnInit {
       expandCode: false,
       title: 'Batch Card with list',
       demoCode: `
-      <div class="rightSideSectionCards ui card sb-course-batch-card">
-    <div class="header batchCardAlign p-16">
-        <span class="header headerBatchCard">Batches</span>
-    </div>
-    <div class="content">
-        <div class="description annannouncement-description">
-            <div class="ui divided list">
-                <div class="item">
-                    <i class="calendar outline icon"></i>
-                    <div class="content width-100">
-                        <button class="ui right floated sb-btn sb-btn-outline-primary sb-btn-normal">Enroll</button>
-                        <span class="header pb-5">  6th May 2019 -  </span>
-                        <div class="description">Created by Creation </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <i  class="icon right-floated cursor-pointer write"></i>
-                    <i class="calendar outline icon"></i>
-                    <div class="content width-100">
-                        <span class="header pb-5">  7th May 2019 -  </span>
-                        <div class="description">Created by Creation </div>
-                    </div>
-                </div>
+      <div class="ui card sb-batch-card">
+      <div class="p-16">
+        <h4 class="sb-batch-card-title">Batches</h4>
+      </div>
+      <div class="sb-batch-card-content">
+        <div class="ui divided list">
+        <div class="item">
+            <i class="calendar outline icon"></i>
+            <div class="content width-100">
+            <button class="ui right floated sb-btn sb-btn-outline-primary sb-btn-normal">Enroll</button>
+            <span class="header pb-5"> 6th May 2019 - </span>
+            <div class="description">Created by Creation </div>
             </div>
         </div>
-    </div>
-    <button class="ui bottom attached sb-btn sb-btn-primary sb-left-icon-btn sb-btn-md no-border">
+        <div class="item">
+            <i class="icon right-floated cursor-pointer write"></i>
+            <i class="calendar outline icon"></i>
+            <div class="content width-100">
+            <span class="header pb-5"> 7th May 2019 - </span>
+            <div class="description">Created by Creation </div>
+            </div>
+        </div>
+        </div>
+      </div>
+      <button class="ui bottom attached sb-btn sb-btn-primary sb-left-icon-btn sb-btn-md no-border">
         <i class="add icon"></i>Create Batch
-    </button>
-</div>
+      </button>
+    </div>
       `,
       copyCode: `
-      <div class="rightSideSectionCards ui card sb-course-batch-card">
-    <div class="header batchCardAlign p-16">
-        <span class="header headerBatchCard">Batches</span>
-    </div>
-    <div class="content">
-        <div class="description annannouncement-description">
-            <div class="ui divided list">
-                <div class="item">
-                    <i class="calendar outline icon"></i>
-                    <div class="content width-100">
-                        <button class="ui right floated sb-btn sb-btn-outline-primary sb-btn-normal">Enroll</button>
-                        <span class="header pb-5">  6th May 2019 -  </span>
-                        <div class="description">Created by Creation </div>
-                    </div>
-                </div>
-                <div class="item">
-                <i  class="icon right-floated cursor-pointer write"></i>
-                 <i class="calendar outline icon"></i>
-                 <div class="content width-100">
-                     <span class="header pb-5">  7th May 2019 -  </span>
-                     <div class="description">Created by Creation </div>
-                 </div>
-             </div>
+      <div class="ui card sb-batch-card">
+      <div class="p-16">
+        <h4 class="sb-batch-card-title">Batches</h4>
+      </div>
+      <div class="sb-batch-card-content">
+        <div class="ui divided list">
+        <div class="item">
+            <i class="calendar outline icon"></i>
+            <div class="content width-100">
+            <button class="ui right floated sb-btn sb-btn-outline-primary sb-btn-normal">Enroll</button>
+            <span class="header pb-5"> 6th May 2019 - </span>
+            <div class="description">Created by Creation </div>
             </div>
         </div>
-    </div>
-    <button class="ui bottom attached sb-btn sb-btn-primary sb-left-icon-btn sb-btn-md no-border">
+        <div class="item">
+            <i class="icon right-floated cursor-pointer write"></i>
+            <i class="calendar outline icon"></i>
+            <div class="content width-100">
+            <span class="header pb-5"> 7th May 2019 - </span>
+            <div class="description">Created by Creation </div>
+            </div>
+        </div>
+        </div>
+      </div>
+      <button class="ui bottom attached sb-btn sb-btn-primary sb-left-icon-btn sb-btn-md no-border">
         <i class="add icon"></i>Create Batch
-    </button>
-</div>
+      </button>
+    </div>
       `
     },
     {
       expandCode: false,
       title: 'Batch Card without list',
       demoCode: `
-      <div class="rightSideSectionCards ui card sb-course-batch-card">
-    <div class="header batchCardAlign p-16">
-        <span class="header headerBatchCard">Batches</span>
-    </div>
-    <div class="content">
-        <div class="description annannouncement-description">
-            <div class="ui divided list">
-                <div class="content my-16">
-                    <div class="sb-no-batch-found">No Batch Found</div>
-                </div>
-            </div>
-          </div>
+      <div class="ui card sb-batch-card">
+      <div class="p-16">
+        <h4 class="sb-batch-card-title">Batches</h4>
+      </div>
+      <div class="sb-batch-card-content">
+        <div class="my-16">
+          <div class="sb-no-batch-found">No Batch Found</div>
         </div>
+      </div>
     </div>
-</div>
       `,
       copyCode: `
-      <div class="rightSideSectionCards ui card sb-course-batch-card">
-    <div class="header batchCardAlign p-16">
-        <span class="header headerBatchCard">Batches</span>
-    </div>
-    <div class="content">
-        <div class="description annannouncement-description">
-            <div class="ui divided list">
-                <div class="content my-16">
-                    <div class="sb-no-batch-found">No Batch Found</div>
-                </div>
-            </div>
-          </div>
+      <div class="ui card sb-batch-card">
+      <div class="p-16">
+        <h4 class="sb-batch-card-title">Batches</h4>
+      </div>
+      <div class="sb-batch-card-content">
+        <div class="my-16">
+          <div class="sb-no-batch-found">No Batch Found</div>
         </div>
+      </div>
     </div>
-</div>
       `
     }
   ];
