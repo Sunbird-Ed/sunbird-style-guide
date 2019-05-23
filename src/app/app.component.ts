ba752124459017486685762991237b11c5c7912a
prajwal88 <prajwal.hj@compassitesinc.com>
2019-05-23 16:11:31 +0530

    Take a tour for portal site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { ShepherdService } from 'angular-shepherd';
+import { steps as defaultSteps, defaultStepOptions} from './data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   showSidebar = true;
   title = 'docs';
+
+  constructor(private shepherdService: ShepherdService) { }
+
+  ngAfterViewInit() {
+    this.shepherdService.defaultStepOptions = defaultStepOptions;
+    this.shepherdService.disableScroll = true;
+    this.shepherdService.modal = true;
+    this.shepherdService.confirmCancel = false;
+    this.shepherdService.addSteps(defaultSteps);
+    this.shepherdService.start();
+  }
 }
