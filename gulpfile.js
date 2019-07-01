2549ba542a52e7073225c0edd99f38c6876b94d0
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-07-01 18:46:14 +0530

    Issue #000 task: Component CSS generation

--- gulpfile.js
+++ gulpfile.js
@@ -6,41 +6,52 @@ sass.compiler = require('node-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 path = require('path');
-var header = require('gulp-header');
+const header = require('gulp-header');
 
-gulp.task('import', function() {
-    return gulp.src(['./src/assets/styles/**/*.scss','!./src/assets/styles/*.scss','!./src/assets/styles/mixins/**/*.scss'])
-      .pipe(header('@import \'../mixins/mixins\';\n'))
-      .pipe(header('@import \'../variables\';\n'))
-      .pipe(gulp.dest('./src/assets/temp'));
+/* Temp Folder - Copy all other files with imports appended at head*/
+gulp.task('append-import', function() {
+  return gulp.src(['./src/assets/styles/**/*.scss','!./src/assets/styles/*.scss','!./src/assets/styles/mixins/**/*.scss'])
+  .pipe(header('@import \'../mixins/mixins\';\n'))
+  .pipe(header('@import \'../variables\';\n'))
+  .pipe(gulp.dest('./src/assets/temp'));
 });
-
+/* Temp Folder - Copy files of level 1*/
 gulp.task('copy-style', function() {
-    return gulp.src(['./src/assets/styles/styles.scss','./src/assets/styles/_variables.scss','./src/assets/styles/legacy.scss','./src/assets/styles/global.scss'])
-      .pipe(gulp.dest('./src/assets/temp'));
+  return gulp.src(['./src/assets/styles/styles.scss','./src/assets/styles/_variables.scss','./src/assets/styles/legacy.scss','./src/assets/styles/global.scss'])
+  .pipe(gulp.dest('./src/assets/temp'));
 });
+/* Temp Folder - Copy mixins folder*/
 gulp.task('copy-mixins', function() {
-    return gulp.src(['./src/assets/styles/mixins/**/*'])
-      .pipe(gulp.dest('./src/assets/temp/mixins'));
+  return gulp.src(['./src/assets/styles/mixins/**/*'])
+  .pipe(gulp.dest('./src/assets/temp/mixins'));
 });
 
-gulp.task('scss', function () {
-    return gulp.src('./src/assets/temp/**/*.scss')
-      .pipe(sourcemaps.init())
-      .pipe(rename(function(path) { //since sass() wont compile _partials files we are removing it here
-        path.basename = path.basename[0] == '_' ? path.basename.substr(1) : path.basename
-      }))
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(sourcemaps.write('./'))
-      .pipe(rename(function (file) {
-        let parentFolder = path.dirname(file.dirname)
-        file.dirname = path.join(parentFolder, 'css');//generated file extension
-      }))
-      .pipe(gulp.dest('./src/assets/dist/'));
+/* Generate Components CSS files from temp folder */
+gulp.task('scss-components', function () {
+  return gulp.src(['./src/assets/temp/**/*.scss','!./src/assets/temp/mixins/**/*.scss','!./src/assets/temp/*.scss'])
+  .pipe(sourcemaps.init())
+  .pipe(rename(function(path) { //since sass() wont compile _partials files we are removing it here
+    path.basename = path.basename[0] == '_' ? path.basename.substr(1) : path.basename
+  }))
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(sourcemaps.write('./'))
+  .pipe(rename(function (file) {
+    const parentFolder = path.dirname(file.dirname);
+    file.dirname = path.join(parentFolder, 'components'); //generated file extension
+  }))
+  .pipe(gulp.dest('./src/assets/dist/'));
+});
+/* Generate Main CSS files from temp folder */
+gulp.task('scss-main', function () {
+  return gulp.src(['./src/assets/styles/styles.scss'])
+  .pipe(sourcemaps.init())
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(gulp.dest('./src/assets/dist/'));
 });
 
+/* Watch SCSS files */
 gulp.task('watch', function() {
-    gulp.watch('./src/assets/styles/**/*.scss', ['copy-mixins','copy-style','import','scss']);
+  gulp.watch('./src/assets/styles/**/*.scss', ['copy-style','copy-mixins','append-import','scss-components','scss-main']);
 });
-  
-gulp.task('default', ['copy-mixins','copy-style','import','scss','watch']);
+
+gulp.task('default', ['copy-style','copy-mixins','append-import','scss-components','scss-main','watch']);
