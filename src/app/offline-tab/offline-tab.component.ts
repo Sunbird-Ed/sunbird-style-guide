2a06cf3453cbba8b109748f450f52a7250249c05
sri-pusuluri <srini@zool.in>
2019-08-07 15:14:36 +0530

    offline tab ui update

--- src/app/offline-tab/offline-tab.component.ts
+++ src/app/offline-tab/offline-tab.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy, Inject, Renderer2} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-offline-tab',
   templateUrl: './offline-tab.component.html',
   styleUrls: ['./offline-tab.component.scss']
 })
-export class OfflineTabComponent implements OnInit {
+export class OfflineTabComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document, private router: Router, private renderer: Renderer2) {}
 
   ngOnInit() {
+    this.renderer.addClass(this.document.body, 'hideLeftTopBars');
+  }
+  ngOnDestroy(): void {
+    this.renderer.removeClass(this.document.body, 'hideLeftTopBars');
   }
 
 }
