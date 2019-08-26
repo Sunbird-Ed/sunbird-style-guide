2e2dc79ccca11ffb72e1ae4d5f1d3a8d62167683
vinodkumar303 <vinodkumar@zool.in>
2019-08-26 15:02:38 +0530

    account recovery copy code added in TS file

--- src/app/account-recovery/account-recovery.component.ts
+++ src/app/account-recovery/account-recovery.component.ts
@@ -10,20 +10,42 @@ export class AccountRecoveryComponent implements OnInit {
   showUpdateRecoveryIDModal;
   updateRecoverymodal;
   pageTitle = 'Modals';
-  UpdateRecoveryIDModalCode = `<sui-modal *ngIf="showUpdateRecoveryIDModal" [mustScroll]="true" [isClosable]="true" [transitionDuration]="0" [size]="'normal'"
-  class="sb-modal" appBodyScroll (dismissed)="showUpdateRecoveryIDModal = !showUpdateRecoveryIDModal" #modal>
+  UpdateRecoveryIDModalCode = ` <sui-modal *ngIf="showUpdateRecoveryIDModal" [mustScroll]="true" [isClosable]="true" [transitionDuration]="0"
+  [size]="'normal'" class="sb-modal" appBodyScroll
+  (dismissed)="showUpdateRecoveryIDModal = !showUpdateRecoveryIDModal" #modal>
   <div class="sb-modal-header">
-    Modal Heading
+    Update Recovery ID
   </div>
   <div class="sb-modal-content">
-    Modal Content
+    <p>
+      A recovery account helps you to regain access to your account if you are locked out or forget your password. Choose the recovery acount type and enter details
+    </p>
+    <div class="d-flex flex-w-wrap">
+      <div class="sb-radio-btn-checkbox sb-radio-btn-primary">
+        <input type="radio" id="email" name="options" checked>
+        <label for="email">Email Address</label>
+      </div>
+      <div class="sb-radio-btn-checkbox sb-radio-btn-primary">
+        <input type="radio" id="phone" name="options">
+        <label for="phone">Phone Number</label>
+      </div>
+    </div>
+    <div class="sb-field">
+      <input class="sb-form-control is-invalid" formcontrolname="email" placeholder="Enter Email Address" type="email">
+      <span class="sb-color-error fxsmall mt-8">
+      This email address is the same as what is linked with your profile
+    </span>
+    </div>
+    <div class="sb-field">
+      <input class="sb-form-control" formcontrolname="phone" placeholder="Enter Phone Number" type="tel">
+      <span class="sb-color-error fxsmall mt-8 hide">
+         Please enter 10 digit phone number 
+        </span>
+    </div>
   </div>
   <div class="sb-modal-actions">
-    <button class="sb-btn sb-btn-normal sb-btn-primary" (click)="doSomething()">
-      Yes
-    </button>
-    <button class="sb-btn sb-btn-normal sb-btn-outline-primary" (click)="doSomething()">
-      No
+    <button class="sb-btn sb-btn-normal sb-btn-primary disabled">
+      Submit
     </button>
   </div>
 </sui-modal>
