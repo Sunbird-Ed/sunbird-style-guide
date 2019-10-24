9edffc864ac9e3990f139d1593eb0207689daf3a
Arti <arti@zool.in>
2019-10-24 18:30:16 +0530

    created offline menu tab bar

--- src/app/offline-menubar/offline-menubar.component.ts
+++ src/app/offline-menubar/offline-menubar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject, OnDestroy } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offline-menubar',
   templateUrl: './offline-menubar.component.html',
   styleUrls: ['./offline-menubar.component.scss']
 })
-export class OfflineMenubarComponent implements OnInit {
+export class OfflineMenubarComponent implements OnInit , OnDestroy {
+  multiSelect1: { name: string; }[];
 
-  constructor() { }
+  constructor(@Inject (DOCUMENT) private document: Document, private router: Router, private renderer: Renderer2) {}
 
   ngOnInit() {
+    this.renderer.addClass(this.document.body, 'hideLeftTopBars');
+    this.multiSelect1 = [{name: 'English'}, {name: 'Hindi'}, {name: 'Urdu'}];
   }
-
+  ngOnDestroy(): void {
+    this.renderer.removeClass(this.document.body, 'hideLeftTopBars');
+  }
 }
