fa78f3627557c9816f91c018958435a6bc65e8b3
Arti <arti@zool.in>
2019-10-25 18:07:31 +0530

    changes in offline-menubar css

--- src/app/offline-test/offline-test.component.ts
+++ src/app/offline-test/offline-test.component.ts
@@ -8,13 +8,46 @@ import { Router } from '@angular/router';
   styleUrls: ['./offline-test.component.scss']
 })
 export class OfflineTestComponent implements OnInit {
+  private selectOption: any;
+  panelOpened = false;
+  selectMedium: { name: string; id: string; value: string; };
 
   constructor(@Inject(DOCUMENT) private document: Document, private router: Router, private renderer: Renderer2) { }
-  multiSelect1: { name: string; }[];
-  panelOpened = false;
   ngOnInit() {
     this.renderer.addClass(this.document.body, 'hideLeftTopBars');
-    this.multiSelect1 = [{name: 'English'}, {name: 'Hindi'}, {name: 'Urdu'}];
+    // this.selectOption = [{name: 'English'}, {name: 'Hindi'}, {name: 'Urdu'}];
+    this.selectOption = [
+      {
+        name: 'English',
+        value: '0'
+      }, {
+        name: 'Hindi',
+        value: '1'
+      }, {
+        name: 'Urdu',
+        value: '2'
+      }, {
+        name: 'Marathi',
+        value: '3'
+      }
+      , {
+        name: 'Kannada',
+        value: '4'
+      }
+      , {
+        name: 'Telugu',
+        value: '5'
+      }
+      , {
+        name: 'Tamil',
+        value: '6'
+      }
+      , {
+        name: 'Malayalam',
+        value: '7'
+      }
+    ];
+    this.selectMedium = this.selectOption[0];
   }
 
 }
