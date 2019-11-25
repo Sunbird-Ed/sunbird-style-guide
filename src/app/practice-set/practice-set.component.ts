bd937a249a299efca133381b61a828c761cf2c48
sri-pusuluri <srini@zool.in>
2019-11-25 18:29:16 +0530

    Practice set & back bar UI changes

--- src/app/practice-set/practice-set.component.ts
+++ src/app/practice-set/practice-set.component.ts
@@ -1,25 +1,60 @@
-import { Component, OnInit,  OnDestroy, Inject, Renderer2 } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
-import { Router } from '@angular/router';
-
+import { Component, OnInit, OnDestroy, Inject, Renderer2 } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-practice-set',
-  templateUrl: './practice-set.component.html',
-  styleUrls: ['./practice-set.component.scss']
+  selector: "app-practice-set",
+  templateUrl: "./practice-set.component.html",
+  styleUrls: ["./practice-set.component.scss"]
 })
 export class PracticeSetComponent implements OnInit {
   showNormalModal;
   uploadFiles = false;
-
-  constructor(@Inject(DOCUMENT) private document: Document, private router: Router, private renderer: Renderer2) { }
-
+  selectOutcomeOption: any = [];
+  selectLevelOption: any = [];
+  selectLicenseOption: any = [];
+  selectLicenceOption1: any;
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router,
+    private renderer: Renderer2
+  ) {}
   ngOnInit() {
-    this.renderer.addClass(this.document.body, 'hideLeftTopBars');
+    this.renderer.addClass(this.document.body, "hideLeftTopBars");
+    this.selectOutcomeOption = [
+      {
+        name: "outcome1",
+        value: "0"
+      },
+      {
+        name: "outcome2",
+        value: "1"
+      }
+    ];
+    this.selectLevelOption = [
+      {
+        name: "level1",
+        value: "0"
+      },
+      {
+        name: "level2",
+        value: "1"
+      }
+    ];
+    this.selectLicenseOption = [
+      {
+        name: "CC by 4.0",
+        value: "0"
+      },
+      {
+        name: "CC by 4.1",
+        value: "1"
+      }
+    ];
   }
   uploadDocument() {
-  this.uploadFiles = !this.uploadFiles;
- }
- uploadModal() {
-  this.showNormalModal = !this.showNormalModal;
- }
+    this.uploadFiles = !this.uploadFiles;
+  }
+  uploadModal() {
+    this.showNormalModal = !this.showNormalModal;
+  }
 }
