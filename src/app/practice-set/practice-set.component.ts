d6c65f47bad9380d9a0e2df6a0c223df2c57b384
Arti <arti@zool.in>
2019-11-25 15:09:21 +0530

    #Issue(SB-15924) UI screens for CBSE contribution add loader gif and styling progressbar

--- src/app/practice-set/practice-set.component.ts
+++ src/app/practice-set/practice-set.component.ts
@@ -10,11 +10,43 @@ import { Router } from '@angular/router';
 export class PracticeSetComponent implements OnInit {
   showNormalModal;
   uploadFiles = false;
-
+  selectOutcomeOption: any = [];
+  selectLevelOption: any = [];
+  selectLicenseOption: any = [];
   constructor(@Inject(DOCUMENT) private document: Document, private router: Router, private renderer: Renderer2) { }
 
   ngOnInit() {
     this.renderer.addClass(this.document.body, 'hideLeftTopBars');
+    this.selectOutcomeOption = [
+      {
+        name: 'outcome1',
+        value: '0'
+      },
+      {
+        name: 'outcome2',
+        value: '1'
+       }
+    ];
+    this.selectLevelOption = [
+      {
+        name: 'level1',
+        value: '0'
+      },
+      {
+        name: 'level2',
+        value: '1'
+       }
+    ];
+    this.selectLicenseOption = [
+      {
+        name: 'CC by 4.0',
+        value: '0'
+      },
+      {
+        name: 'CC by 4.1',
+        value: '1'
+       }
+     ];
   }
   uploadDocument() {
   this.uploadFiles = !this.uploadFiles;
