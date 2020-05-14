8081661cb8c370cdde3563c875185e8e930de91b
ArtiSingh23 <arti@zool.in>
2020-05-14 16:32:22 +0530

    add modals inside the schooling V2 screens and images

--- src/app/schooling-home-groups/schooling-home-groups.component.ts
+++ src/app/schooling-home-groups/schooling-home-groups.component.ts
@@ -9,6 +9,11 @@ import { Router } from '@angular/router';
 })
 export class SchoolingHomeGroupsComponent implements OnInit {
   dropdownContent: boolean = true;
+  showRemoveModal;
+  showDismissAdminModal;
+  showMakeAdminModal;
+  showDeleteModal;
+  showPastMemberModal;
   constructor(@Inject(DOCUMENT) private document: Document, private router: Router, private renderer: Renderer2) {}
 
   ngOnInit() {
